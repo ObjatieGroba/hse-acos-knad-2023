504f6f65cd30f9610f77a280c00e1fa98f36e2c8
Igor Mineev <igron99@mail.ru>
2023-02-11 12:40:22 +0300

    Add sem7

---
+++ sm7-01.asm
@@ -0,0 +1,56 @@
+	li a7, 7
+	ecall
+	
+	fsd fa0, dbl, t1
+	
+	
+	la a0, dbl
+	addi a0, a0, 4
+	lw a0, (a0)
+	li a7, 35
+	ecall
+	
+	lw a0, dbl
+	li a7, 35
+	ecall
+	
+	li a7, 11
+	li a0, '\n'
+	ecall
+	
+	
+	la a0, dbl
+	addi a0, a0, 7
+	lbu a0, (a0)
+	
+	srli a0, a0, 7
+	li a7, 35
+	ecall
+	
+	li a7, 11
+	li a0, '\n'
+	ecall
+	
+	
+	
+	la a0, dbl
+	addi a0, a0, 4
+	lw a0, (a0)
+	
+	slli a0, a0, 1  # Стерли знак
+	srli a0, a0, 22  # Убираем остаток мантисы 32 + 1 - x = 11
+	
+	addi a0, a0, -1203
+	li a7, 1
+	ecall
+	
+	li a7, 11
+	li a0, '\n'
+	ecall
+	
+	
+	
+	.data
+dbl:
+	.double 0.0
+	
